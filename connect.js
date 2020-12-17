5df898b9e1b7f08eaae0d9a9293b61cc2a78bb12
Priyanshu Jha <priyanshujha98@gmail.com>
2020-12-17 16:44:25 +0530

    second commit

--- connect.js
+++ connect.js
@@ -1,12 +1,44 @@
 
-
 $(document).ready(function(){
 
 
-	$("#target").submit(function(event) {
+	$("#signin").submit(function(event) {
 		event.preventDefault();
+
+		console.log(JSON.stringify({unmae:$('#user').val(), pwd:$('#pass').val() }))
+		$.ajax({
+            url: "https://0g02hvzc46.execute-api.ap-south-1.amazonaws.com/dev/emailauth",
+            data: JSON.stringify({unmae:$('#user').val(), pwd:$('#pass').val() }),
+            type: 'POST',
+           	crossDomain: true,
+            contentType: 'application/json',
+            dataType: "json",
+            headers: {
+				      
+				      'Content-Type': 'application/json'
+				    },
+			
+            success: function(data){
+              alert(data)
+              console.log(data);
+              
+            }
+
+            
+
+          });
+
 	});
 
 })
 
 
+/*
+headers: {
+   						 "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+   						 "Access-Control-Allow-Methods" : ["GET, POST, PUT, DELETE"],
+   						 "Access-Control-Allow-Headers" : ["Content-Type, Accept"]
+    					//"Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS 
+  					}
+
+ */
